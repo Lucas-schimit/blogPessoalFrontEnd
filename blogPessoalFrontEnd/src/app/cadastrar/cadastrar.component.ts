6e37cdd317d1a3f937fdb1d440eb1813d71a63b8
Lucas-schimit <lucas_schimit@outlook.com>
2021-03-23 18:11:38 -0300

    chore(consumo-parte1): Foi criado o consumo parte1) #1

--- blogPessoalFrontEnd/src/app/cadastrar/cadastrar.component.ts
+++ blogPessoalFrontEnd/src/app/cadastrar/cadastrar.component.ts
@@ -1,4 +1,7 @@
+import { AuthService } from './../service/auth.service';
+import { User } from './../model/User';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastrar',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarComponent implements OnInit {
 
-  constructor() { }
+  User: User = new User
+  confirmarSenha: string
+  tipoUsuario: string
 
-  ngOnInit(): void {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit(){
+    window.scroll(0,0)
+
   }
 
+  confirmSenha(event:  any){
+    this.confirmSenha = event.targe.value
+  }
+
+  tipoUser(event: any){
+    this.tipoUsuario = event.targe.value
+  }
+
+  cadastra(){
+    this.User.tipo = this.tipoUsuario
+
+    if(this.User.senha != this.confirmSenha){
+      alert('A senhas estão incorretas.')
+    } else{
+      this.authService.cadastrar(this.User).subscribe((resp: User) =>{
+        this.User = resp
+        this.router.navigate(['/entrar'])
+        alert('Usuário cadastrado com sucesso!')
+      })
+    }
+  }
 }
